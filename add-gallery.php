3d71e2bb28d01b764ee4b4180583bfc8874c58db
Saidalixon Sobirov <saidalixon_sobirov@mail.ru>
2021-06-20 16:53:16 +0500

    Admin Dashboard for Universty website

---
+++ add-gallery.php
@@ -0,0 +1,279 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
+  { 
+header('location:index.php');
+}
+else{
+
+// For adding post  
+if(isset($_POST['submitgallery']))
+{
+$gallery_desc=$_POST['gallery_desc'];
+
+$arr = explode(" ",$gallery_desc);
+$url=implode("-",$arr);
+$imgfile=$_FILES["photo_gal"]["name"];
+// get the image extension
+$extension = substr($imgfile,strlen($imgfile)-4,strlen($imgfile));
+// allowed extensions
+$allowed_extensions = array(".jpg","jpeg",".png",".gif",".JPG","JPEG",".PNG",".GIF");
+// Validation for allowed extensions .in_array() function searches an array for a specific value.
+if(!in_array($extension,$allowed_extensions))
+{
+echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+}
+else
+{
+//rename the image files
+$imgnewfile=md5($imgfile).$extension;
+// Code for move image into directory
+move_uploaded_file($_FILES["photo_gal"]["tmp_name"],"../gallery/images/".$imgnewfile);
+
+$status=1;
+$query=mysqli_query($con,"insert into tblgallery(gallery_desc,photo_gal) values('$gallery_desc','$imgnewfile')");
+if($query)
+{
+$msg="Photo successfully added ";
+}
+else{
+$error="Something went wrong .-- Please try again.";    
+} 
+
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <!-- App title -->
+        <title>| Add Photo</title>
+
+        <!-- Summernote css -->
+        <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
+
+        <!-- Select2 -->
+        <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Jquery filer css -->
+        <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+        <script src="assets/js/modernizr.min.js"></script>
+ <script>
+function getSubCat(val) {
+  $.ajax({
+  type: "POST",
+  url: "get_subcategory.php",
+  data:'catid='+val,
+  success: function(data){
+    $("#subcategory").html(data);
+  }
+  });
+  }
+  </script>
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Top Bar Start -->
+           <?php include('includes/topheader.php');?>
+            <!-- ========== Left Sidebar Start ========== -->
+             <?php include('includes/leftsidebar.php');?>
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="page-title-box">
+                                    <h4 class="page-title">Add Photo </h4>
+                                    <ol class="breadcrumb p-0 m-0">
+                                        <li>
+                                            <a href="#">Photo</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Add Photo </a>
+                                        </li>
+                                        <li class="active">
+                                            Add Photo
+                                        </li>
+                                    </ol>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end row -->
+
+<div class="row">
+<div class="col-sm-6">  
+<!---Success Message--->  
+<?php if($msg){ ?>
+<div class="alert alert-success" role="alert">
+<strong>Well done!</strong> <?php echo htmlentities($msg);?>
+</div>
+<?php } ?>
+
+<!---Error Message--->
+<?php if($error){ ?>
+<div class="alert alert-danger" role="alert">
+<strong>Oh snap!</strong> <?php echo htmlentities($error);?></div>
+<?php } ?>
+
+
+</div>
+</div>
+
+
+                        <div class="row">
+                            <div class="col-md-10 col-md-offset-1">
+                                <div class="p-6">
+                                    <div class="">
+<form name="addpost" method="post" enctype="multipart/form-data">
+ <div class="form-group m-b-20">
+<label for="exampleInputEmail1">Gallery description</label>
+<input type="text" class="form-control" id="gallery_desc" name="gallery_desc" placeholder="" required>
+</div>
+
+<!-- // bu yerda category bilan subcategory// -->
+         
+
+<!-- <div class="row">
+<div class="col-sm-12">
+ <div class="card-box">
+<h4 class="m-b-30 m-t-0 header-title"><b>Post Details</b></h4>
+<textarea class="summernote" name="postdescription" required></textarea>
+</div>
+</div>
+</div> -->
+
+
+<div class="row">
+<div class="col-sm-12">
+ <div class="card-box">
+<h4 class="m-b-30 m-t-0 header-title"><b>Upload Image</b></h4>
+<input type="file" class="form-control" id="photo_gal" name="photo_gal"  required>
+</div>
+</div>
+</div>
+
+
+<button type="submit" name="submitgallery" class="btn btn-success waves-effect waves-light">Add Photo</button>
+ <button type="button" class="btn btn-danger waves-effect waves-light">Discard</button>
+                                        </form>
+                                    </div>
+                                </div> <!-- end p-20 -->
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+           <?php include('includes/footer.php');?>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+        <!-- Select 2 -->
+        <script src="../plugins/select2/js/select2.min.js"></script>
+        <!-- Jquery filer js -->
+        <script src="../plugins/jquery.filer/js/jquery.filer.min.js"></script>
+
+        <!-- page specific js -->
+        <script src="assets/pages/jquery.blog-add.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script>
+
+            jQuery(document).ready(function(){
+
+                $('.summernote').summernote({
+                    height: 240,                 // set editor height
+                    minHeight: null,             // set minimum height of editor
+                    maxHeight: null,             // set maximum height of editor
+                    focus: false                 // set focus to editable area after initializing summernote
+                });
+                // Select2
+                $(".select2").select2();
+
+                $(".select2-limiting").select2({
+                    maximumSelectionLength: 2
+                });
+            });
+        </script>
+  <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+
+    
+
+
+    </body>
+</html>
+<?php } ?>
---
+++ includes/leftsidebar.php
@@ -0,0 +1,71 @@
+<div class="left side-menu">
+    <div class="sidebar-inner slimscrollleft">
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <ul>
+                <li class="menu-title">Navigation</li>
+                <li class="has_sub">
+                    <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                    
+                </li>
+                
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Category </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="add-category.php">Add Category</a></li>
+                        <li><a href="manage-categories.php">Manage Category</a></li>
+                    </ul>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span>Sub Category </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="add-subcategory.php">Add Sub Category</a></li>
+                        <li><a href="manage-subcategories.php">Manage Sub Category</a></li>
+                    </ul>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Posts </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="add-post.php">Add Posts</a></li>
+                        <li><a href="manage-posts.php">Manage Posts</a></li>
+                        <li><a href="trash-posts.php">Trash Posts</a></li>
+                    </ul>
+                </li>
+                
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span>Trainers </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="add-trainer.php">Add trainer</a></li>
+                        
+                    </ul>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Pages </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="aboutus.php">Кафедра ҳақида</a></li>
+                        <!-- <li><a href="contactus.php">Contact us</a></li> -->
+                        <li><a href="uslubiyishlar.php">Услубий ишлар</a></li>
+                        <li><a href="ilmiytadqiqotishlar.php">Илмий-тадқиқот ишлар</a></li>
+                        <li><a href="manaviymarifiyishlar.php">Маънавий-маърифий ишлар</a></li>
+                        
+                    </ul>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Фотолавҳалар </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="add-gallery.php">Расм қўшиш</a></li>
+                        <!--    <li><a href="unapprove-comment.php">Waiting for Approval </a></li>
+                        <li><a href="manage-comments.php">Approved Comments</a></li> -->
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+        <div class="clearfix"></div>
+        <div class="help-box">
+            <h5 class="text-muted m-t-0">For Help ?</h5>
+            <p class=""><span class="text-custom">Email:</span> <br/> seriusTeam@gmail.com</p>
+        </div>
+    </div>
+    <!-- Sidebar -left -->
+</div>
